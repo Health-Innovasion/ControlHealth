26981d71a6bf6d2d0503b56922eff0b7f51f88a4
NRivera07 <57815899+NRivera07@users.noreply.github.com>
2023-10-20 18:09:23 -0600

    Fix style quotes and flow

--- src/Components/Quotes/Quote.jsx
+++ src/Components/Quotes/Quote.jsx
@@ -1,46 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import './Quote.css';
-import { getCitas } from '../../redux/action/action';
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react'
+import './Quote.css'
+import { getCitas } from '../../redux/action/action'
+import { useSelector } from 'react-redux'
+import img from '../../Assets/Images/icono-user.png'
+import { Col, Row } from 'react-bootstrap'
 
 const Quote = () => {
-    const { currentUser } = useSelector((state) => state.user);
-    const [citas, setCitas] = useState([]);
+  const { currentUser } = useSelector((state) => state.user)
+  const [citas, setCitas] = useState([])
 
-    useEffect(() => {
-        const unsubscribe = getCitas(currentUser.uid, (citasData) => {
-            setCitas(citasData);
-        });
+  useEffect(() => {
+    const unsubscribe = getCitas(currentUser.uid, (citasData) => {
+      setCitas(citasData)
+    })
 
-        return () => {
-            // Limpia la suscripción cuando el componente se desmonta
-            unsubscribe();
-        };
-    }, [currentUser.uid]);
-    
-    if (citas.length === 0) {
-        return (
-            <div className="sidebar">
-                No hay citas disponibles.
-            </div>
-        );
+    return () => {
+      // Limpia la suscripción cuando el componente se desmonta
+      unsubscribe()
     }
+  }, [currentUser.uid])
+  function getStatusClass(status) {
+    const statusClasses = {
+      'En revisión': 'warning',
+      'success': 'success',
+    };
+  
+    return statusClasses[status] || 'error';
+  }
+  
 
-    return (
-        <div className="sidebar">
-            {citas.map((cita, index) => {
-                const citaData = cita.data.data.cita;
-                const medicoData = cita.data.data.medico;
-                return (
-                    <div className="quote-card" key={index}>
-                        <h1>{citaData.name}</h1>
-                        <span>{citaData.date}</span>
-                        <span>{medicoData.displayName}</span>
-                    </div>
-                );
-            })}
-        </div>
-    );
-};
+  if (citas.length === 0) {
+    return <div className="sidebar">No hay citas disponibles.</div>
+  }
 
-export default Quote;
+  return (
+    <div className="sidebar">
+      {citas?.length &&
+        citas.map((cita, index) => {
+          const citaData = cita.data.data.cita
+          const status = cita.data.data.status
+          return (
+            <div className="d-flex quote-card" key={index}>
+              <img className="img-doctor-quote" src={img} alt="" />
+
+              <div className="col-quote">
+                <span className="title-quote">Doctor:</span>
+                <span className="title-quote">Estado:</span>
+              </div>
+              <div className="col-quote">
+                <span>{citaData.doctor.userName}</span>
+                <span className={`status-color ${getStatusClass(status)}`}>
+                  {status}
+                </span>
+              </div>
+              <div className="col-quote">
+                <span className="title-quote">Fecha:</span>
+
+                <span className="title-quote">Hora:</span>
+              </div>
+              <div className="col-quote">
+                <span>{citaData.date}</span>
+
+                <span>{citaData.date}</span>
+              </div>
+            </div>
+          )
+        })}
+    </div>
+  )
+}
+
+export default Quote
