71bac208ca5f2be4062c15a09ba9d7d25631a131
Donald Alvarez <73081566+Donald-Alvarez@users.noreply.github.com>
2023-10-20 22:02:34 -0600

    Style navbar

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,12 +4,15 @@ import { Card, Button } from 'react-bootstrap';
 const CustomCard = ({ title, content, image, buttonText, buttonOnClick }) => {
   return (
     <Card>
+      {image && <Card.Img variant="top" src={image} />}
       <Card.Body>
-        {title && <Card.Title>{title}</Card.Title>} {/* Mostrar el título si está presente */}
-          <Button variant="primary" >
-            jjhjhjh
+        {title && <Card.Title>{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+        {buttonText && (
+          <Button variant="primary" onClick={buttonOnClick}>
+            {buttonText}
           </Button>
-
+        )}
       </Card.Body>
     </Card>
   );
